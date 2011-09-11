b4b3478b444a1fe1ec95794f0c7d9bd63af11431
David Winter <winda002@student.otago.ac.nz>
2011-09-11 21:01:40 +1200

    using colourBrewer clours to shade tree branches

--- scripts/plot.r
+++ scripts/plot.r
@@ -38,18 +38,20 @@ assign_colour <- function(s) {
 }
 
 
-d <- read.dna('filtered_renamed_ali.fasta', 'fasta')
+d <- read.dna('../trees/filtered_renamed_ali.fasta', 'fasta')
 tr <- nj(dist.dna(d, 'K81'))
 
 ids <- unlist(with(tr, lapply(tip.label, get_id)))
 genera <- unlist(with(tr, lapply(tip.label, get_genus)))
 species <- unlist(with(tr, lapply(tip.label, get_species)))
 
+c.gen <- ifelse(genera=="Nothofagus", "#1B9E77", "#D95F02")
+c.species <- unlist(lapply(species, assign_colour))
+
 write.csv(cbind(ids, c.species), "species.csv", row.names=F, quote=F)
 write.csv(cbind(ids, c.gen), "genera.csv", row.names=F, quote=F)
 
-c.gen <- ifelse(genera=="Nothofagus", "347235", "brown")
-c.species <- unlist(lapply(species, assign_colour))
+
 plot(tr, x.lim = 3, show.tip.label=F) 
 x <- rep(1, length(species))
 barplot(c(NA, x), add = TRUE, horiz = TRUE, space = 0, offset = 1, axes=F, col=c.species, border=NA)

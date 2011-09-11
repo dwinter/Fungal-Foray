05a813a579419d5ba9652fa91235a880e331afa1
David Winter <winda002@student.otago.ac.nz>
2011-09-11 21:24:02 +1200

    cleaned up the plot.r script and added comments

--- scripts/plot.r
+++ scripts/plot.r
@@ -1,4 +1,18 @@
+####################################################
+# Mainly an archive of interactive session plotting trees estimated
+# from the sequences
+####################################################
+
 library(ape)
+#Read in the aligned sequences, make a tree and write it out
+d <- read.dna('../trees/filtered_renamed_ali.fasta', 'fasta')
+tr <- nj(dist.dna(d, 'K81'))
+write.tree('filtered_nj.tre')
+
+
+#I'm going to use iTol to plot that tree, so need csv files assigning
+#id to shade
+
 
 get_id <- function(s)  {
   unlist(strsplit(s, ' '))[1]
@@ -13,14 +27,12 @@ get_species <- function(s) {
    paste(f[2], " ", f[3])
 }
 
-#This is really stupid, but don't enough R to
-#do it better...
-
-# I do ;-)
+#Thanks Peter Green for more sensible way of doing this
+#Colours are generated from RColorBrewer
 
 my_colours <- c(
 
-  `Pinus   radiata` = '#A52A2A',
+  `Pinus   radiata` = '#D95F02',
   `Nothofagus   menziesii` = '#006D2C',
   `Nothofagus   fusca` = '#31A354',
   `Nothofagus   solandri` = '#74C476',
@@ -29,12 +41,6 @@ my_colours <- c(
 )
 
 assign_colour <- function(s) return(my_colours[s])
-
-# nb (I'd also use rgb() rather than direct hex values)
-
-d <- read.dna('../trees/filtered_renamed_ali.fasta', 'fasta')
-tr <- nj(dist.dna(d, 'K81'))
-
 ids <- unlist(with(tr, lapply(tip.label, get_id)))
 genera <- unlist(with(tr, lapply(tip.label, get_genus)))
 species <- unlist(with(tr, lapply(tip.label, get_species)))
@@ -45,7 +51,8 @@ c.species <- unlist(lapply(species, assign_colour))
 write.csv(cbind(ids, c.species), "species.csv", row.names=F, quote=F)
 write.csv(cbind(ids, c.gen), "genera.csv", row.names=F, quote=F)
 
-
+#I tried to use R to make a plot similar to the iTol one in R 
+# it didn't work
 plot(tr, x.lim = 3, show.tip.label=F) 
 x <- rep(1, length(species))
 barplot(c(NA, x), add = TRUE, horiz = TRUE, space = 0, offset = 1, axes=F, col=c.species, border=NA)
